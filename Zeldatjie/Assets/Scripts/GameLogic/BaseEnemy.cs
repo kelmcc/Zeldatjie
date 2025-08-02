5d800067b9926e99e6b50c432e2a3f2e14ce12cd
KellyM <kelly.mccarter@carry1st.com>
2025-08-02 14:28:49 +0200

    Building out prefabs

--- Zeldatjie/Assets/Scripts/GameLogic/BaseEnemy.cs
+++ Zeldatjie/Assets/Scripts/GameLogic/BaseEnemy.cs
@@ -4,7 +4,10 @@ namespace Zeldatjie.Gameplay
 {
     public class BaseEnemy : MonoBehaviour
     {
+        [SerializeField] EnemyDataBase _enemyData;
         [SerializeField] private int _health;
         [SerializeField] private int _attackPower;
+        
+        public bool IsAlive => _health > 0;
     }
 }
---
+++ Zeldatjie/Assets/Scripts/GameLogic/EnemyDataBase.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Zeldatjie.Gameplay
+{
+    [CreateAssetMenu(fileName = "EnemyDataBase", menuName = "ScriptableObjects/EnemyDataBase", order = 0)]
+    public class EnemyDataBase : ScriptableObject
+    {
+        
+    }
+}
